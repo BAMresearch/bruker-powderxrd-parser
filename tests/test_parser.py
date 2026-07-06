96e78938fe91fe3c690db8df5d093d0f1e0dac41
LucasZimm <lucas.zimmermann@bam.de>
2026-07-06 11:47:32 +0200

    checked for dependencie issues and added test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,8 +7,21 @@ class TestBrukerPowderXRDParser:
         collection = CollectionType()
         parser.parse([], collection, logger)
 
-        assert len(collection.attached_objects) == 2
+        assert len(collection.attached_objects) == 0
+        assert len(collection.relationships) == 0
+
+
+class TestBrukerFileXRDParser:
+    def test_parse(self, parser):
+        collection = CollectionType()
+        parser.parse(
+            [r"tests\data\D6Pm\A52-B128-S_20-140_0.008_4s_17h_D6Pm.brml"],
+            collection,
+            logger,
+        )
+
+        assert len(collection.attached_objects) == 1
         objects = list(collection.attached_objects.values())
-        assert objects[0].name == "Synthesis"
-        assert objects[1].name == "Measurement"
-        assert len(collection.relationships) == 1
+        assert objects[0].name == "Experiment0"
+        assert objects[0].voltage == 40
+        assert len(collection.relationships) == 0
